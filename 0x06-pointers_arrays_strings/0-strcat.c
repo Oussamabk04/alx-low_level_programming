bbc2f7aea3a1171c29877802508b9b9d2bfbb438
Oussamabk04 <oussamaballouk37@gmail.com>
2023-10-18 16:45:50 -0700

    103-infinite_add.c

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -1,34 +1,30 @@
 #include "main.h"
-
 /**
-  *_strcat - main function of the prototype
-  *
-  * @src: Concatenation parameter
-  *
-  * @dest: Concatenation parameter
-  *
-  * Return: dest
-  */
-
+ * _strcat - concatenates two strings
+ * @dest: input value
+ * @src: input value
+ *
+ * Return: void
+ */
 char *_strcat(char *dest, char *src)
 {
-int k, r;
+	int i;
+	int j;
 
-k = 0;
-r = 0;
+	i = 0;
+	while (dest[i] != '\0')
+	{
+		i++;
+	}
+	j = 0;
+	while (src[j] != '\0')
+	{
+		dest[i] = src[j];
+		i++;
+		j++;
+	}
 
-while (dest[k] != '\0')
-{
-	k++;
-}
-
-while (src[r] != '\0')
-{
-	dest[k] = src[r];
-	k++;
-	r++;
-}
-dest[k] = '\0';
-return (dest);
+	dest[i] = '\0';
+	return (dest);
 }
 
