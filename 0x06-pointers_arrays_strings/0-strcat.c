af802d2bd8f37fa0810a6287fd7f58eb90c5c77b
Oussamabk04 <oussamaballouk37@gmail.com>
2023-10-18 17:38:48 -0700

    0-strcat.c

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -1,34 +1,24 @@
 #include "main.h"
-
+#include <stdio.h>
 /**
-  *_strcat - main function of the prototype
-  *
-  * @src: Concatenation parameter
-  *
-  * @dest: Concatenation parameter
-  *
-  * Return: dest
-  */
+ * *_strcat - concatenates two strings
+ * @dest: pointer destination
+ * @src: pointer source
+ * Return: void
+ */
 
 char *_strcat(char *dest, char *src)
 {
-int k, r;
+int a = -1, i;
+for (i = 0; dest[i] != '\0'; i++)
+;
 
-k = 0;
-r = 0;
+do {
+	a++;
+	dest[i] = src[a];
+	i++;
+} while (src[a] != '\0');
 
-while (dest[k] != '\0')
-{
-	k++;
-}
-
-while (src[r] != '\0')
-{
-	dest[k] = src[r];
-	k++;
-	r++;
-}
-dest[k] = '\0';
 return (dest);
 }
 
